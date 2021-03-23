a32ed2352025b85f81c725929b996f9a25834c85
ellykhodaie <elahekhodaie@yahoo.com>
2021-03-23 20:42:09 +0430

    added getters and setters to rectangle class

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -11,4 +11,21 @@ public class Rectangle {
         return this.width * this.height;
     }
 
+
+    public float getHeight() {
+        return height;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
 }
