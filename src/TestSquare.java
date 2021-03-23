31d3e57866aea1519947dcb47bbabe031edf71ce
Sina <stavakkoli1999@gmail.com>
2021-03-23 21:00:03 +0430

    test square compute area added

--- src/TestSquare.java
+++ src/TestSquare.java
@@ -1,2 +1,12 @@
+import org.junit.Test;
+import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 public class TestSquare {
+    @Test
+    public void testSquareComputeArea() {
+        Shape square = new Square(10);
+        assertEquals(100, square.computeArea(), 0);
+    }
+	
 }
