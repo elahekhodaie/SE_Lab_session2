a8edeaf6633429c6def4f9bed96d15e0e35a1de1
Sina <stavakkoli1999@gmail.com>
2021-03-23 21:01:03 +0430

    test set square side added

--- src/TestSquare.java
+++ src/TestSquare.java
@@ -8,5 +8,12 @@ public class TestSquare {
         Shape square = new Square(10);
         assertEquals(100, square.computeArea(), 0);
     }
-	
+
+    @Test
+    public void testSetSquareSide() {
+        Square square = new Square(7);
+        square.setSide(5);
+        assertEquals(5, square.getSide(), 0);
+
+    }
 }
