79f5ee2ebbe22285e558cfa992c51e5d9599bb5f
Sina <stavakkoli1999@gmail.com>
2021-03-23 21:08:03 +0430

    compute area for square added

--- src/Square.java
+++ src/Square.java
@@ -1,10 +1,16 @@
 public class Square {
     private float side;
+    public Square(float side) {
+       this.setSide(side);
+    }
     public void setSide(float side) {
         this.side = side;
     }
     public float getSide() {
         return side;
+    }
+    public double computeArea(){
+        return getSide() * getSide();
     }
 
 }
