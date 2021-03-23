16b091c0501e3076e284e9594d42f7cacfbf5378
Sina <stavakkoli1999@gmail.com>
2021-03-23 21:07:03 +0430

    square setter & getter added

--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,10 @@
 public class Square {
+    private float side;
+    public void setSide(float side) {
+        this.side = side;
+    }
+    public float getSide() {
+        return side;
+    }
+
 }
